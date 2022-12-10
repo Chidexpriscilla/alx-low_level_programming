62d8c8c82b3319893d2614bab7e920a167262b8d
Chidexpriscilla <chimmaobiekezie@gmail.com>
2022-12-10 04:27:01 -0800

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Printing in C
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return: Always 0 (Successful code check)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
