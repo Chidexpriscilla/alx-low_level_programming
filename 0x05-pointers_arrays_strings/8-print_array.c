4585d7f4463acbe8e260c2d9ee36828edfb530dc
Chidexpriscilla <117783690+Chidexpriscilla@users.noreply.github.com>
2022-12-20 03:28:08 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,37 +1,24 @@
 #include"main.h"
 
-
-
 /**
+ * print_array - prints n elements of an array of integers.
  *
- *  * _puts - prints a string, followed by a newline to stdout
+ * @n: elements parameter input
+ * @a: string parameter input
  *
- *   *
- *
- *    * @str: string parameter to print
- *
- *     *
- *
- *      * Return: Nothing
- *
- *       */
+ * Return: Nothing
+ */
 
-
-
-void _puts(char *str)
-
+void print_array(int *a, int n)
 {
+	int i;
 
-		while (*str != '\0')
-
-				{
-
-							_putchar(*str + 0);
-
-									++str;
-
-										}
-
-			_putchar('\n');
-
+	for (i = 0; i < n; ++i)
+	{
+		if (i != (n - 1))
+			printf("%d, ", a[i]);
+		else
+			printf("%d", a[i]);
+	}
+	printf("\n");
 }
